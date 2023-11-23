95f668b05353844d0eb1eb22e98f6fb13d6d9a61
KaterynaModna <modnakatya@gmail.com>
2023-11-23 09:43:04 +0100

    made button change

--- src/components/FormHeader.js
+++ src/components/FormHeader.js
@@ -5,6 +5,27 @@ import TourList from "./TourList";
 export default function FormHeader() {
   const [formData, setFormData] = useState([]);
   const [searchData, setSearchData] = useState('');
+  const [inputData, setInputData] = useState('');
+
+  // const handleInputChange = async (event) => {
+  //   setSearchData(event.target.value)
+  //   const url = `https://travel-advisor.p.rapidapi.com/locations/v2/auto-complete?query=${event.target.value}&lang=en_US&units=km`;
+  //   const options = {
+  //     method: 'GET',
+  //     headers: {
+  //       'X-RapidAPI-Key': '93db34e6admsh559889a966b871ep1e9b7ajsnffaa82ddb7ba',
+  //       'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+  //     }
+  //   }
+  //   try {
+  //     const response = await fetch(url, options);
+  //     const result = await response.text();
+  //     console.log(result);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  //   };
+
 
   const getValue = async () => {
     const url = `https://travel-advisor.p.rapidapi.com/locations/search?query=${searchData}&limit=30&lang=en_US&offset=0&location_id=1&sort=relevance&lang=en_US`;
@@ -17,6 +38,7 @@ export default function FormHeader() {
       }
     };
 
+
     try {
       const response = await fetch(url, options);
       const result = await response.json();
@@ -28,37 +50,36 @@ export default function FormHeader() {
         category: item.result_object?.category?.name || '',
         address: item.result_object?.address_obj?.address || '',
       }));
-
       setFormData(tours);
     } catch (error) {
       console.error(error);
     }
   };
 
-  useEffect(() => {
-    getValue();
-  }, [searchData]);
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
     getValue();
   };
+  
 
   return (
     <div>
       <form className="complex_input" onSubmit={onSubmitHandler}>
-        <FormControl fullWidth>
+        <FormControl fullWidth >
           <TextField id="standard-search"
-          type="search"
-          variant="standard"
+            type="search"
+            variant="standard"
             required
             placeholder="Write down the city"
             value={searchData}
-            onChange={(event) => setSearchData(event.target.value)}
+            onChange={e => setSearchData(e.target.value)}
           />
         </FormControl>
 
-        <button className="search_header_button" type="submit">search</button>
+        <button  className="search_header_button" type="submit">search</button>
+
+
       </form>
       <div className="tour-list-container">
         <TourList data={formData} />
@@ -66,7 +87,6 @@ export default function FormHeader() {
     </div>
   );
 }
-
 {/* <div className="select_container">
 <i className="fa-solid fa-location-dot"></i>
 <input className="select_item" name="name_item" id="name_item" required>
