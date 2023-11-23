ec99358bc6c3f412e03cacdbfc9f43c2b013f665
KaterynaModna <modnakatya@gmail.com>
2023-11-23 18:12:59 +0100

    made form submit in state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,55 @@
-import { useState } from "react"
-
+import { useState } from "react";
 
 export default function UserForm() {
+  const [userData, setUserData] = useState({});
 
-const [userData, setUserData] = useState({});
-const onChangeHandler = (e) =>{
-const {value, name} = e.target;
+  const onChangeHandler = (e) => {
+    const { value, name } = e.target;
     setUserData({
-        ...userData,
-        [name]: value
-    })
-}
- 
+      ...userData,
+      [name]: value
+    });
+  };
 
-return (
-    <form >
-    <div className="form_wrapper">
-        <input onChange={onChangeHandler}  className="form_input"  type="text" id="user_name" placeholder="First name" required/>
-        <input onChange={onChangeHandler} className="form_input"   type="tel" id="user_phone" placeholder="+ 38 (_ _ _) _ _ _   _ _   _ _" required/>
-        <input onChange={onChangeHandler} className="form_input"   type="email" id="user_email" placeholder="yourmail@gmail.com" required/>
-            <button className="form_button" type="button">Book Consultation</button>
-            </div>
-        </form>
-    )
-    }
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log("Form submitted with data:", userData);
+  };
+
+  return (
+    <form onSubmit={onSubmitHandler}>
+      <div className="form_wrapper">
+        <input
+          onChange={onChangeHandler}
+          className="form_input"
+          type="text"
+          id="user_name"
+          name="user_name"
+          placeholder="First name"
+          required
+        />
+        <input
+          onChange={onChangeHandler}
+          className="form_input"
+          type="tel"
+          id="user_phone"
+          name="user_phone"
+          placeholder="+ 38 (_ _ _) _ _ _   _ _   _ _"
+          required
+        />
+        <input
+          onChange={onChangeHandler}
+          className="form_input"
+          type="email"
+          id="user_email"
+          name="user_email"
+          placeholder="yourmail@gmail.com"
+          required
+        />
+        <button className="form_button" type="submit">
+          Book Consultation
+        </button>
+      </div>
+    </form>
+  );
+}
